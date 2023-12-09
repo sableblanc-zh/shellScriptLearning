c99f23962d309df65d1c9f8e616e5708bc14b980
Shabai <153232331+sableblanc-zh@users.noreply.github.com>
2023-12-09 13:57:23 +0100

    Update countWordsInFile.sh
    The second option to execute the count

--- countWordsInFile.sh
+++ countWordsInFile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-printf "## COUNTING WORDS SCRIPT LAUNCHING ##\n\n"
+printf "## COUNTING WORDS SCRIPT 2 LAUNCHING ##\n\n"
 
 filepath="$1"
 #Check if user has defined an input filepath
@@ -7,9 +7,16 @@ if [ ! -z $filepath ]
 then
 	if [ -f $filepath ]
 	then
- 		#Get the name of the file
 		filename=$(basename $filepath | cut -d. -f1)
-		cat $filepath | tr -d [:punct:] | tr -s [:space:] | tr [:space:] "\n" | sort -i | uniq -ic | awk '{print $2, $1}' | sort -i > "${filename}_count.txt"
+		file_content=$(cat $filepath)
+		file_str=$(echo $file_content | tr '[:upper:]' '[:lower:]' | tr -d [:punct:] | tr -s [:space:])
+
+  		# Another way to get word count with awk to make a dictionnary
+		echo $file_str | tr [:space:] "\n" | awk '{count[$1]++} 
+			END {for (word in count)
+			{
+				print word,count[word]}
+			}' | sort -k2,2nr > "${filename}_words_count.txt"
 	else
 		echo "The file $filepath doesn't exist"
 		exit 1
@@ -18,4 +25,3 @@ else
 	echo "Please enter an valid path of a file as parameter"
 	exit 1
 fi
-
