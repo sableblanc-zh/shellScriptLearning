28e5ccd5a9ea27691458ed57585769537e8e2507
Shabai <153232331+sableblanc-zh@users.noreply.github.com>
2023-12-09 14:01:49 +0100

    Update countWordsInFile.sh

--- countWordsInFile.sh
+++ countWordsInFile.sh
@@ -7,8 +7,10 @@ if [ ! -z $filepath ]
 then
 	if [ -f $filepath ]
 	then
- 		#Get the name of the file
+ 		#Get the name of the file to create the output word count results
 		filename=$(basename $filepath | cut -d. -f1)
+
+  		#word count result will be sorted by the alphabetic order of all occurent words, following by their count.
 		cat $filepath | tr -d [:punct:] | tr -s [:space:] | tr [:space:] "\n" | sort -i | uniq -ic | awk '{print $2, $1}' | sort -i > "${filename}_count.txt"
 	else
 		echo "The file $filepath doesn't exist"
